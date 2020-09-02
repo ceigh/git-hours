fd8aa653a3f144083a0ccf3710e7f9caf5f11694
Artjom Löbsack <ceigh@pm.me>
2020-09-02 15:09:01 +0500

    feat: get email from config

--- hours.c
+++ hours.c
@@ -3,15 +3,19 @@
 #include <linux/limits.h>
 #include <git2.h>
 
+const char *get_default_email();
+
 int main() {
+  const char *default_email = get_default_email();
+
   char cwd[PATH_MAX];
+  getcwd(cwd, sizeof(cwd));
+
   git_repository *repo = NULL;
   git_revwalk *walker = NULL;
 
-  getcwd(cwd, sizeof(cwd));
-  printf("cwd: %s\n\n", cwd);
-
   git_libgit2_init();
+
   git_repository_open(&repo, cwd);
   git_revwalk_new(&walker, repo);
   git_revwalk_sorting(walker, GIT_SORT_REVERSE);
@@ -21,10 +25,10 @@ int main() {
   while(!git_revwalk_next(&oid, walker)) {
     git_commit *commit = NULL;
     git_commit_lookup(&commit, repo, &oid);
+    const char *commit_email = (*git_commit_author(commit)).email;
 
-    printf("%li %s\n",
-      git_commit_time(commit),
-      (*git_commit_author(commit)).email);
+    if (*commit_email != *default_email) continue;
+    printf("%li %s\n", git_commit_time(commit), commit_email);
 
     git_commit_free(commit);
   }
@@ -34,4 +38,19 @@ int main() {
   git_libgit2_shutdown();
 
   return 0;
+}
+
+const char *get_default_email() {
+  const char *email = NULL;
+  git_config *cfg = NULL;
+  git_config *cfg_snapshot = NULL;
+
+  git_libgit2_init();
+  git_config_open_default(&cfg);
+  git_config_snapshot(&cfg_snapshot, cfg);
+  git_config_get_string(&email, cfg_snapshot, "user.email");
+
+  git_config_free(cfg);
+  git_libgit2_shutdown();
+  return email;
 }
