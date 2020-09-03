0726fe7f7167473d5f07acf5359365feefc73054
Artjom Löbsack <ceigh@pm.me>
2020-09-03 23:04:19 +0500

    feat: parse command line opts

--- hours.c
+++ hours.c
@@ -1,8 +1,10 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <linux/limits.h>
 #include <git2.h>
+#include <getopt.h>
 
 #define MAX_DIFF_MINUTES 120
 #define FIRST_COMMIT_MINUTES 120
@@ -28,7 +30,10 @@ const char *get_default_email() {
   return email;
 }
 
-int get_hours(const char *path_to_repo, const char *author_email, const int **commits) {
+void get_hours(
+  long *hours, long *commits,
+  const char *path_to_repo, const char *author_email
+) {
   git_repository *repo = NULL;
   git_revwalk *walker = NULL;
 
@@ -42,9 +47,9 @@ int get_hours(const char *path_to_repo, const char *author_email, const int **co
   git_revwalk_push_head(walker);
 
   git_oid oid;
-  int commits_total = 0;
   float minutes_total = 0;
-  long int prev_time = 0;
+  long commits_total = 0;
+  long prev_time = 0;
 
   while(!git_revwalk_next(&oid, walker)) {
     git_commit *commit = NULL;
@@ -60,7 +65,7 @@ int get_hours(const char *path_to_repo, const char *author_email, const int **co
     /* now we can start to count commits and time */
     commits_total++;
 
-    const long int time = author.when.time;
+    const long time = author.when.time;
     /* difference between commits in minutes */
     const float diff = (time - prev_time) / 60.0;
     prev_time = time;
@@ -82,26 +87,58 @@ int get_hours(const char *path_to_repo, const char *author_email, const int **co
   }
 
   /* to balance last commit minutes */
-  if (minutes_total >= FIRST_COMMIT_MINUTES) minutes_total -= FIRST_COMMIT_MINUTES;
+  if (minutes_total >= FIRST_COMMIT_MINUTES)
+    minutes_total -= FIRST_COMMIT_MINUTES;
 
   /* free */
   git_revwalk_free(walker);
   git_repository_free(repo);
   git_libgit2_shutdown();
 
-  *commits = &commits_total;
-  return (int)(minutes_total / 60);
+  /* point results */
+  *hours = (long)(minutes_total / 60);
+  *commits = commits_total;
 }
 
-int main() {
-  const char *default_email = get_default_email();
+void parse_opts(int argc, char **argv, char **email, char **path) {
+  int c;
+  opterr = 0;
 
+  while ((c = getopt(argc, argv, "e:")) != -1) {
+    switch (c) {
+      case 'e':
+        *email = optarg;
+        break;
+      case '?':
+        if (optopt == 'e')
+          fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+        else if (isprint(optopt))
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        else
+          fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+        exit(EXIT_FAILURE);
+      default:
+        abort();
+    }
+  }
+
+  *path = argv[optind];
+}
+
+int main(int argc, char **argv) {
+  char *email_opt_val = NULL, *path_opt_val = NULL;
+  parse_opts(argc, argv, &email_opt_val, &path_opt_val);
+
+  const char *email = email_opt_val == NULL
+    ? get_default_email() : email_opt_val;
   char cwd[PATH_MAX];
   getcwd(cwd, sizeof(cwd));
+  const char *path = path_opt_val == NULL
+    ? cwd : path_opt_val;
 
-  const int *commits = NULL;
-  const int hours = get_hours(cwd, default_email, &commits);
+  long commits = 0, hours = 0;
+  get_hours(&hours, &commits, path, email);
 
-  printf("%s\t%d\t%d\n", default_email, hours, *commits);
+  printf("%s\t%li\t%li\n", email, hours, commits);
   exit(EXIT_SUCCESS);
 }
